5e6ebe8264e373c8857ae61f08535e87c178ead3
AlbertoSpano <alberto.spano.1961@gmail.com>
2019-04-09 21:30:38 +0200

    First commit

--- RigheFileTxt/FileTxt.Designer.vb
+++ RigheFileTxt/FileTxt.Designer.vb
@@ -22,12 +22,17 @@ Partial Class FileTxt
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.grdFiles = New System.Windows.Forms.DataGridView()
+        Me.btnScegli = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtEstensione = New System.Windows.Forms.TextBox()
         Me.NomeFile = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Righe = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.FullPath = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.btnEsporta = New System.Windows.Forms.Button()
+        CType(Me.grdFiles, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'OpenFileDialog1
@@ -37,23 +42,63 @@ Partial Class FileTxt
         Me.OpenFileDialog1.Filter = "File txt|*.txt"
         Me.OpenFileDialog1.Multiselect = True
         '
-        'DataGridView1
+        'grdFiles
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NomeFile, Me.Righe, Me.FullPath})
-        Me.DataGridView1.Location = New System.Drawing.Point(363, 38)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(321, 370)
-        Me.DataGridView1.TabIndex = 0
+        Me.grdFiles.AllowUserToAddRows = False
+        Me.grdFiles.AllowUserToDeleteRows = False
+        Me.grdFiles.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grdFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdFiles.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NomeFile, Me.Righe, Me.FullPath})
+        Me.grdFiles.Location = New System.Drawing.Point(337, 44)
+        Me.grdFiles.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.grdFiles.Name = "grdFiles"
+        Me.grdFiles.ReadOnly = True
+        Me.grdFiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.grdFiles.Size = New System.Drawing.Size(572, 462)
+        Me.grdFiles.TabIndex = 0
+        '
+        'btnScegli
+        '
+        Me.btnScegli.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnScegli.Location = New System.Drawing.Point(29, 84)
+        Me.btnScegli.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnScegli.Name = "btnScegli"
+        Me.btnScegli.Size = New System.Drawing.Size(259, 30)
+        Me.btnScegli.TabIndex = 1
+        Me.btnScegli.Text = "Scegli ..."
+        Me.btnScegli.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Estensione file:"
+        '
+        'txtEstensione
+        '
+        Me.txtEstensione.Location = New System.Drawing.Point(158, 44)
+        Me.txtEstensione.MaxLength = 6
+        Me.txtEstensione.Name = "txtEstensione"
+        Me.txtEstensione.Size = New System.Drawing.Size(130, 23)
+        Me.txtEstensione.TabIndex = 3
+        Me.txtEstensione.Text = ".txt"
         '
         'NomeFile
         '
         Me.NomeFile.HeaderText = "Nome file"
         Me.NomeFile.Name = "NomeFile"
         Me.NomeFile.ReadOnly = True
+        Me.NomeFile.Width = 400
         '
         'Righe
         '
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        Me.Righe.DefaultCellStyle = DataGridViewCellStyle1
         Me.Righe.HeaderText = "Righe"
         Me.Righe.Name = "Righe"
         Me.Righe.ReadOnly = True
@@ -65,22 +110,47 @@ Partial Class FileTxt
         Me.FullPath.ReadOnly = True
         Me.FullPath.Visible = False
         '
+        'btnEsporta
+        '
+        Me.btnEsporta.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnEsporta.Enabled = False
+        Me.btnEsporta.Location = New System.Drawing.Point(29, 134)
+        Me.btnEsporta.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnEsporta.Name = "btnEsporta"
+        Me.btnEsporta.Size = New System.Drawing.Size(259, 30)
+        Me.btnEsporta.TabIndex = 4
+        Me.btnEsporta.Text = "Esporta elenco"
+        Me.btnEsporta.UseVisualStyleBackColor = True
+        '
         'FileTxt
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.ClientSize = New System.Drawing.Size(922, 525)
+        Me.Controls.Add(Me.btnEsporta)
+        Me.Controls.Add(Me.txtEstensione)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnScegli)
+        Me.Controls.Add(Me.grdFiles)
+        Me.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MinimumSize = New System.Drawing.Size(600, 300)
         Me.Name = "FileTxt"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "File txt"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.grdFiles, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents grdFiles As DataGridView
+    Friend WithEvents btnScegli As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtEstensione As TextBox
     Friend WithEvents NomeFile As DataGridViewTextBoxColumn
     Friend WithEvents Righe As DataGridViewTextBoxColumn
     Friend WithEvents FullPath As DataGridViewTextBoxColumn
+    Friend WithEvents btnEsporta As Button
 End Class
--- RigheFileTxt/FileTxt.vb
+++ RigheFileTxt/FileTxt.vb
@@ -1,3 +1,56 @@
 ﻿Public Class FileTxt
 
+    Private Sub btnScegli_Click(sender As Object, e As EventArgs) Handles btnScegli.Click
+
+        Dim ext As String = Me.txtEstensione.Text
+        ext = ext.TrimStart("*")
+
+        ext = If(ext.StartsWith("."), "", ".") + ext
+
+        Me.OpenFileDialog1.DefaultExt = ext
+        Me.OpenFileDialog1.FileName = "*" + ext
+        Me.OpenFileDialog1.Filter = String.Format("File (*{0})|{0}", ext)
+
+        If Me.OpenFileDialog1.ShowDialog = DialogResult.OK Then
+
+            Me.grdFiles.Rows.Clear()
+
+            For Each f As String In Me.OpenFileDialog1.FileNames
+                Me.grdFiles.Rows.Add(IO.Path.GetFileName(f), IO.File.ReadAllLines(f).Count, f)
+            Next
+
+            Me.btnEsporta.Enabled = grdFiles.Rows.Count > 0
+
+        End If
+
+    End Sub
+
+    Private Sub btnEsporta_Click(sender As Object, e As EventArgs) Handles btnEsporta.Click
+
+        Me.Cursor = Cursors.WaitCursor
+
+        Try
+
+            Dim ftmp As String = IO.Path.Combine(System.IO.Path.GetTempPath(), IO.Path.GetRandomFileName + ".csv")
+
+            Dim result As New List(Of String)
+            result.Add(String.Format("{0}{1}{2}{1}{3}", "Nome file", vbTab, "Righe", "Percorso completo"))
+
+            For Each d As DataGridViewRow In grdFiles.Rows
+                result.Add(String.Format("{0}{1}{2}{1}{3}", d.Cells(0).Value, vbTab, d.Cells(1).Value, d.Cells(2).Value))
+            Next
+
+            IO.File.WriteAllLines(ftmp, result.ToArray)
+
+            System.Diagnostics.Process.Start(ftmp)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+        Me.Cursor = Cursors.Default
+
+    End Sub
+
 End Class
